260d8d4a6c490a6abe94023b73a69d9ebbcac3fd
fabiangarciadelabarra-byte <fabiangarciadelabarra@gmail.com>
2026-05-06 11:23:52 -0500

    Update prices.js

--- api/prices.js
+++ api/prices.js
@@ -1,12 +1,21 @@
-export default async function handler(request, response) {
+export const config = {
+  runtime: "edge",
+};
+
+export default async function handler(req) {
   try {
     const btc = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json").then(r => r.json());
 
-    return response.status(200).json({
-      price: btc.bpi.USD.rate_float,
-      time: btc.time.updatedISO
-    });
+    return new Response(
+      JSON.stringify({
+        price: btc.bpi.USD.rate_float,
+        time: btc.time.updatedISO
+      }),
+      { status: 200 }
+    );
   } catch (error) {
-    return response.status(500).json({ error: "Error fetching BTC price" });
+    return new Response(JSON.stringify({ error: "Error fetching BTC price" }), {
+      status: 500
+    });
   }
 }
